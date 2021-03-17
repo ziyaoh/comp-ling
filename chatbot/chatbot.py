e7faa9ebdf5fed6399835bac7766d9acdfaaeeec
ziyaoh <yaozi326@outlook.com>
2021-03-17 15:57:24 -0400

    chatbot

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -7,6 +7,8 @@ import numpy as np
 from preprocess import *
 from tqdm import tqdm
 
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 hyper_params = {
@@ -27,8 +29,23 @@ def train(model, train_loader, optimizer, experiment):
     """
     # TODO: Write the training loop here, save trained model weights if needed
     model = model.train()
+    loss_func = torch.nn.CrossEntropyLoss(ignore_index=0)
     with experiment.train():
-        pass
+        for epoch in range(hyper_params["num_epochs"]):
+            print("training for epoch", epoch)
+            for batch in tqdm(train_loader):
+                data = batch["data"].to(DEVICE)
+                label = batch["label"].to(DEVICE)
+                # Forward + Backward + Optimize
+                logits = model(input_ids=data)
+                pred = torch.flatten(logits, start_dim=0, end_dim=1)
+                label = torch.flatten(label)
+
+                optimizer.zero_grad()
+                loss = loss_func(pred, label)
+                loss.backward()
+                optimizer.step()
+                experiment.log_metric("loss", loss.detach().cpu())
 
 
 def test(model, test_loader, experiment):
@@ -39,10 +56,26 @@ def test(model, test_loader, experiment):
     :param experiment: comet.ml experiment object
     """
     # TODO: Write the testing loop and calculate perplexity
+    total_loss = 0
+    word_count = 0
     model = model.eval()
     with experiment.validate():
+        with torch.no_grad():
+            for batch in tqdm(test_loader):
+                data = batch["data"].to(DEVICE)
+                label = batch["label"].to(DEVICE)
+                length = batch["length"].to(DEVICE)
+
+                loss = model(input_ids=data, labels=label)
+                num = torch.sum(length)
+                # print(loss)
+                # print(num)
+                total_loss += (loss * num)
+                word_count += num
+        # Log perplexity to Comet.ml using experiment.log_metric
+        perplexity = torch.exp(total_loss / word_count)
         print("perplexity:", perplexity)
-        experiment.log_metric("perplexity", perplexity)
+        experiment.log_metric("perplexity", perplexity.cpu())
 
 
 def interactive(input, tokenizer, model, top_k=10, ntok=20):
@@ -64,7 +97,12 @@ def interactive(input, tokenizer, model, top_k=10, ntok=20):
     when printing out the response.
     """
     # TODO: Write the generation function for interacting with trained model
-    response = None
+    prompt_ids = tokenizer.encode(input)
+    data = torch.LongTensor([tokenizer.bos_token_id] + prompt_ids + [tokenizer.sep_token_id]).to(DEVICE)
+
+    result = model.generate(input_ids=data, max_length=ntok, do_sample=True, top_k=top_k, eos_token_id=tokenizer.eos_token_id, forced_eos_token_id=tokenizer.eos_token_id)
+
+    response = tokenizer.decode(result[0][data.shape[0]: -1])
     print(response)
 
 
@@ -88,10 +126,14 @@ if __name__ == "__main__":
     experiment.log_parameters(hyper_params)
 
     # Load the GPT2 Tokenizer, add any special token if needed
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
     # Intialized the pretrained GPT-2 model and optimizer
+    model = GPT2LMHeadModel.from_pretrained('gpt2')
+    optimizer = torch.optim.Adam(model.parameters(), lr = hyper_params['learning_rate'])
 
     # Load the train, test DataLoader NOTE: Parse the data using GPT2 tokenizer
+    train_loader, test_loader = load_dataset((args.train_file, args.test_file), tokenizer, hyper_params["batch_size"])
 
     if args.load:
         model.load_state_dict(torch.load('model.pt'))
--- chatbot/preprocess.py
+++ chatbot/preprocess.py
@@ -1,9 +1,68 @@
 from transformers import *
 from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
 import torch
 
 
-def load_dataset(fn, tokenizer, batch_size, window_size):
+class ChatbotDataset(Dataset):
+
+    def __init__(self, file_path, tokenizer):
+        bos, sep, eos = tokenizer.bos_token_id, tokenizer.sep_token_id, tokenizer.eos_token_id
+
+        with open(file_path, "r") as f:
+            sentences = f.readlines()
+
+        sentences = [sent.strip() for sent in sentences]
+
+        self.all_data = list()
+        self.all_label = list()
+        for sentence in sentences:
+            pair = sentence.split("\t")
+            if len(pair) != 2:
+                continue
+            prompt, response = pair
+            prompt = " ".join(prompt.split()[1:])
+
+            prompt_id = tokenizer.encode(prompt)
+            response_id = tokenizer.encode(response)
+
+            data = [bos] + prompt_id + [sep] + response_id
+            label = prompt_id + [sep] + response_id + [eos]
+            assert(len(data) == len(label))
+            self.all_data.append(data)
+            self.all_label.append(label)
+
+    def __len__(self):
+        return len(self.all_data)
+
+    def __getitem__(self, idx):
+        return {
+            "data": self.all_data[idx],
+            "label": self.all_label[idx],
+        }
+
+
+class MyCollator:
+    def __init__(self, data_padding_value, label_padding_value):
+        self.data_padding_value = data_padding_value
+        self.label_padding_value = label_padding_value
+
+    def __call__(self, batch):
+        data, label, length = list(), list(), list()
+        for item in batch:
+            data.append(item["data"])
+            label.append(item["label"])
+            length.append(len(item["data"]))
+        data = pad_sequence(data, batch_first=True, padding_value=self.data_padding_value)
+        label = pad_sequence(label, batch_first=True, padding_value=self.label_padding_value)
+        return {
+            "data": data,
+            "label": label,
+            "length": torch.LongTensor(length),
+        }
+
+
+def load_dataset(fn, tokenizer, batch_size):
     """
     :param fn: filename for the dataset
     :return: (torch.utils.data.DataLoader, torch.utils.data.DataLoader) for
@@ -18,4 +77,13 @@ def load_dataset(fn, tokenizer, batch_size, window_size):
     token to the start and end it, also add a special token between sentence
     and response to differentiate them from each other.
     """
-    pass
+    train_set = ChatbotDataset(fn[0], tokenizer)
+    test_set = ChatbotDataset(fn[1], tokenizer)
+    
+    train_collator = MyCollator(tokenizer.pad_token_id, tokenizer.pad_token_id)
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, collate_fn=train_collator)
+
+    test_collator = MyCollator(tokenizer.pad_token_id, -100)
+    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, collate_fn=test_collator)
+
+    return train_loader, test_loader
